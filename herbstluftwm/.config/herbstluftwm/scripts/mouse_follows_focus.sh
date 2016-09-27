130315fc6f8afed52e8b74f6c298ef5b6bc615a3
Timo Kaufmann <eisfreak7@gmail.com>
2016-09-27 18:59:02 +0200

    Add herbstluftwm config

---
+++ herbstluftwm/.config/herbstluftwm/scripts/mouse_follows_focus.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+while read -r _; do
+	winid="$(herbstclient attr clients.focus.winid)"
+	xwininfo="$(xwininfo -id "$winid")"
+
+	# pos[0] is x of the top-left corner, pos[1] y
+	cornerpos=( $(echo "$xwininfo" | awk '$1=="Corners:"{print $2}' | tr '+-' '  ') )
+	width="$(echo "$xwininfo" | awk '$1=="Width:"{print $2}')"
+	height="$(echo "$xwininfo" | awk '$1=="Height:"{print $2}')"
+
+	eval "$(xdotool getmouselocation --shell)" # now X, Y contain the mouse location
+
+	# check if mouse is outside of the newly focused window
+	# (so the mouse isn't "warped" when the window was selected by mouse movement)
+	if (( X < cornerpos[0] )) || (( X > cornerpos[0] + width)) \
+	|| (( $Y < ${cornerpos[1]} )) || (( $Y > ${cornerpos[1]} + $height)); then
+		(( centerX = ${cornerpos[0]} + ($width/2) ))
+		(( centerY = ${cornerpos[1]} + ($height/2) ))
+		xdotool mousemove $centerX $centerY
+	fi
+done < <(herbstclient --idle '(focus_changed|fullscreen|focus_moved)') # focus_moved is a custom hook
