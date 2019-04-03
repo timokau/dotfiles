79737b1f16f349d5ae34030d5e2c51988ecce91d
Timo Kaufmann <timokau@zoho.com>
2019-04-03 16:29:23 +0200

    Install notmuch python api

--- home/configuration.nix
+++ home/configuration.nix
@@ -54,6 +54,7 @@ with lib; {
       imagemagick # cli image editing
       # for quick python experiments
       (python3.withPackages (pkgs: with pkgs; [
+        notmuch # notmuch python api to sort mails
         # tensorflow # machine learning (currently not available with python 3.7)
         pytest
         # TODO for some reason it is necessary to install this into the
