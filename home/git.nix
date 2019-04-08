962ef95acc449c1af311013ad4e013d0846d06e4
Timo Kaufmann <timokau@zoho.com>
2019-04-08 23:05:53 +0200

    git: add alias for quick worktree creation

--- home/git.nix
+++ home/git.nix
@@ -4,6 +4,10 @@
     enable = true;
     aliases = {
       co = "checkout";
+      # create a quick new feature branch in a separate dir
+      wa = ''
+        "!sh -c 'git worktree prune && git worktree add -b $0 ../$0 ''${1:-upstream/master} && cd ../$0'"
+      '';
       find-merge = ''
         "!sh -c 'commit=$0 && branch=''${1:-HEAD} && (git rev-list $commit..$branch --ancestry-path | cat -n; git rev-list $commit..$branch --first-parent | cat -n) | sort -k2 -s | uniq -f1 -d | sort -n | tail -1 | cut -f2'"
       '';
