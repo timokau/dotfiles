afad2da68801ca48d41e99f6e040b8144fcc0d51
Timo Kaufmann <timokau@zoho.com>
2019-05-02 20:14:13 +0200

    newsboat: add patreon-rss script

--- home/newsboat.nix
+++ home/newsboat.nix
@@ -1,13 +1,8 @@
 { pkgs, ... }:
 {
   programs.newsboat.enable = true;
-  # programs.newsboat.extraConfig = builtins.readFile ./newsboat/config;
   xdg.configFile."newsboat/config".source = ../newsboat/config;
-  # FIXME
-  # xdg.dataFile."newsboat" = {
-  #   source = ~/state/newsboat;
-  #   recursive = true;
-  # };
+  xdg.configFile."newsboat/scripts/patreon-rss.py".source = ../newsboat/patreon-rss.py;
   # newsboat considers this config, I consider it state
   xdg.configFile."newsboat/urls".source = ~/state/rss-urls;
 }
