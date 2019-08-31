a5ccd6b7b6584a16971d306aecf995fbff0bd933
Timo Kaufmann <timokau@zoho.com>
2019-08-31 12:16:31 +0200

    Install pdfpc

--- home/configuration.nix
+++ home/configuration.nix
@@ -53,6 +53,7 @@ with lib; {
         };
       };
     in with pkgs; [
+      pdfpc # latex beamer presentations
       entr # run command on file changes (nicer interface than inotify)
       skim # fzf clone in rust
       vagrant # VM management
