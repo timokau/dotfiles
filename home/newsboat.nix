5a8ed57f0c2e366d61cbc2aa8ca79a6253e272b6
Timo Kaufmann <timokau@zoho.com>
2019-11-09 11:24:37 +0100

    newsboat: remove patreon-rss feed
    Unfortunately they now have cloudflare in front of the unauthorized API,
    blocking access with captchas. Kinda against the point of an API. I
    guess they don't want unauthorized access anymore, which is fair enough.

--- home/newsboat.nix
+++ home/newsboat.nix
@@ -2,7 +2,6 @@
 {
   programs.newsboat.enable = true;
   xdg.configFile."newsboat/config".source = ../newsboat/config;
-  xdg.configFile."newsboat/scripts/patreon-rss.py".source = ../newsboat/patreon-rss.py;
   # newsboat considers this config, I consider it state
   xdg.configFile."newsboat/urls".source = ~/state/rss-urls;
 }
