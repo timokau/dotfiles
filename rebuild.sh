9b711f31ac86fff30cc84b45caf6b86bf2528fa1
Timo Kaufmann <timokau@zoho.com>
2019-11-14 22:40:14 +0100

    Use nix2 in home-manager rebuild

--- rebuild.sh
+++ rebuild.sh
@@ -2,4 +2,4 @@
 (cd nixos; ./apply.sh)
 NIXPKGS="$(nix eval --raw '(import ./nixpkgs.nix)')"
 # set nixpkgs in NIX_PATH explicitly once, then it gets set as the default
-sudo nixos-rebuild -I nixpkgs="$NIXPKGS" switch && home-manager switch
+sudo nixos-rebuild -I nixpkgs="$NIXPKGS" switch && home-manager -2 switch
