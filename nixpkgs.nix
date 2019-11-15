76f07c29e9532a3e1105dfae12a3ca41f4d7ef57
Timo Kaufmann <timokau@zoho.com>
2019-11-15 22:01:08 +0100

    nixpkgs: update
    So that I can test lorri.

--- nixpkgs.nix
+++ nixpkgs.nix
@@ -1,6 +1,6 @@
 let
   # `git ls-remote https://github.com/nixos/nixpkgs-channels nixos-unstable`
-  nixpkgs-rev = "db3beda6b56563777254156351b9bed349cf7d07";
+  nixpkgs-rev = "c96e556ed7b968806974b2b473176c72d8abd0b6";
 in builtins.fetchTarball {
   name = "nixpkgs-${nixpkgs-rev}";
   url = "https://github.com/nixos/nixpkgs/archive/${nixpkgs-rev}.tar.gz";
