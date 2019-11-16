46c7deaf7f1df62ded52e40a00e4c51172644f6c
Timo Kaufmann <timokau@zoho.com>
2019-11-16 11:27:43 +0100

    nixpgks: update
    Lorri improvements, finally a green channel eval again
    (https://hydra.nixos.org/build/106865774).

--- nixpkgs.nix
+++ nixpkgs.nix
@@ -1,6 +1,6 @@
 let
   # `git ls-remote https://github.com/nixos/nixpkgs-channels nixos-unstable`
-  nixpkgs-rev = "c96e556ed7b968806974b2b473176c72d8abd0b6";
+  nixpkgs-rev = "c1966522d7d5fa54db068140d212cba18731dd98";
 in builtins.fetchTarball {
   name = "nixpkgs-${nixpkgs-rev}";
   url = "https://github.com/nixos/nixpkgs/archive/${nixpkgs-rev}.tar.gz";
