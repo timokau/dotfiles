415a952daa1ce8924193c0f3c61cd224574f8ad8
Timo Kaufmann <timokau@zoho.com>
2020-01-24 16:27:03 +0100

    Revert "home: don't install the sage documentation"
    This reverts commit 699a8d2fba5fcf317012930196fd2c6b09c99ff5.
    
    The sage.doc issue was fixed in
    https://github.com/NixOS/nixpkgs/pull/78348.

--- home/configuration.nix
+++ home/configuration.nix
@@ -146,7 +146,7 @@ with pkgs.lib; {
       # nix-store -q --requisites $( home-manager build ) | while read line; do du -hs "$line"; done | uniq | sort -h
       pandoc # convert between markup formats (pandoc -> ghc -> ~1.4G space)
       texlive.combined.scheme-full # latex
-      sage # math software
+      sageWithDoc # math software
     ]) ++ (optionals cfg.graphical [
       libreoffice
       anki # flash cards
