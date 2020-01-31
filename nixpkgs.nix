018850a10cede2a0ddb7613b87b81e25a3e05c41
Timo Kaufmann <timokau@zoho.com>
2020-01-31 17:07:35 +0100

    nixpkgs: update
    My kernel patch has finally made it upstream, so no locally building the
    kernel anymore!

--- nixpkgs.nix
+++ nixpkgs.nix
@@ -1,6 +1,6 @@
 let
   # `git ls-remote https://github.com/nixos/nixpkgs-channels nixos-unstable`
-  nixpkgs-rev = "73a59af35bb72e74c928515aeb6ed6d3f03c9034";
+  nixpkgs-rev = "79713f8a9067d269a00960eeefeaafcadeca9b98";
 in builtins.fetchTarball {
   name = "nixpkgs-${nixpkgs-rev}";
   url = "https://github.com/nixos/nixpkgs/archive/${nixpkgs-rev}.tar.gz";
