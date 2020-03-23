9e92a12152cfde190656722eca7feff98639c8b0
Timo Kaufmann <timokau@zoho.com>
2020-03-23 14:16:30 +0100

    Do the interactive part of rebuilding first
    Importing nixpkgs takes some time, after which the script asks for
    confirmation and a sudo prompt may pop up. Better to do the interactive
    part first so that I can then leave it running unattended.

--- rebuild.sh
+++ rebuild.sh
@@ -1,9 +1,5 @@
 #!/bin/sh
 
-NIXPKGS="$(nix eval --raw '(import ./nixpkgs.nix)')"
-# set nixpkgs in NIX_PATH explicitly once, then it gets encoded as the default in the system configuration
-export NIX_PATH="nixpkgs=$NIXPKGS:$NIX_PATH"
-
 echo "Nixos config diff to be applied:"
 diff --color=always -r /etc/nixos nixos
 echo "Continue?"
@@ -11,6 +7,12 @@ read
 
 # THIS REMOVES THE OLD CONFIG
 sudo install --owner=root --group=root --mode=644 nixos/*.nix nixos/hostname nixos/homeipv6 nixpkgs.nix /etc/nixos
+
+echo "Rebuilding"
+
+NIXPKGS="$(nix eval --raw '(import ./nixpkgs.nix)')"
+# set nixpkgs in NIX_PATH explicitly once, then it gets encoded as the default in the system configuration
+export NIX_PATH="nixpkgs=$NIXPKGS:$NIX_PATH"
 
 # First build both systems using nix 2
 nix build '(with import <nixpkgs/nixos> { }; system)' || exit $?
