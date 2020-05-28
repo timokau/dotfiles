bdf354aa641d565edaecca429f1339339409de46
Timo Kaufmann <timokau@zoho.com>
2020-05-28 13:07:14 +0200

    nixpkgs: update
    The channel had been broken for a while. High time for an update.

--- nixpkgs.nix
+++ nixpkgs.nix
@@ -1,6 +1,6 @@
 let
   # `git ls-remote https://github.com/nixos/nixpkgs-channels nixos-unstable`
-  nixpkgs-rev = "4a302d1b9aab8c767bdae26a1d5bcc5dbdcb796a";
+  nixpkgs-rev = "d34fa6ed4434c5b18f4e4ec4b9a11f5e6f6d2400";
 in builtins.fetchTarball {
   name = "nixpkgs-${nixpkgs-rev}";
   url = "https://github.com/nixos/nixpkgs/archive/${nixpkgs-rev}.tar.gz";
