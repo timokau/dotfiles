c8ab901be1fb288a7b5e9eaae4cc49c14aa7c1c0
Timo Kaufmann <timokau@zoho.com>
2020-09-25 15:23:24 +0200

    git: fix aliases
    Automatically escaped since some home-manager update.

--- home/git.nix
+++ home/git.nix
@@ -2,21 +2,22 @@
 {
   programs.git = {
     enable = true;
+    delta.enable = true;
     aliases = {
       co = "checkout";
       # create a quick new feature branch in a separate dir
       wa = ''
-        "!sh -c 'git worktree prune && git worktree add -b $0 ../$0 ''${1:-upstream/master} && cd ../$0'"
+        !sh -c 'git worktree prune && git worktree add -b $0 ../$0 ''${1:-upstream/master} && cd ../$0'
       '';
       find-merge = ''
-        "!sh -c 'commit=$0 && branch=''${1:-HEAD} && (git rev-list $commit..$branch --ancestry-path | cat -n; git rev-list $commit..$branch --first-parent | cat -n) | sort -k2 -s | uniq -f1 -d | sort -n | tail -1 | cut -f2'"
+        !sh -c 'commit=$0 && branch=''${1:-HEAD} && (git rev-list $commit..$branch --ancestry-path | cat -n; git rev-list $commit..$branch --first-parent | cat -n) | sort -k2 -s | uniq -f1 -d | sort -n | tail -1 | cut -f2'
       '';
       track = ''
-        "!git push --set-upstream timokau \"$(git symbolic-ref --short HEAD)\""
+        !git push --set-upstream timokau "$(git symbolic-ref --short HEAD)"
       '';
-	  bisect-edit = ''
-	    "!sh -c 'git bisect log > /tmp/bisect-log && nvim /tmp/bisect-log && git bisect reset && git bisect replay /tmp/bisect-log'"
-	  '';
+      bisect-edit = ''
+        !sh -c 'git bisect log > /tmp/bisect-log && nvim /tmp/bisect-log && git bisect reset && git bisect replay /tmp/bisect-log'
+      '';
       # better overview, less details
       lg = "log --color --graph --oneline --decorate";
     };
