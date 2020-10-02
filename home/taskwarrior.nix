986fec18868ed2704eff674b0bbe192ac0daf127
Timo Kaufmann <timokau@zoho.com>
2020-10-02 12:49:30 +0200

    taskwarrior: configure relative recurrence

--- home/taskwarrior.nix
+++ home/taskwarrior.nix
@@ -12,6 +12,7 @@
     source = ../taskwarrior/.config/task;
   };
   home.file.".taskrc".source = ../taskwarrior/taskrc; # points to the ~/.config folder
+  home.file.".local/share/task/hooks/on-modify.relative-recur".source = ../taskwarrior/hooks/on-modify.relative-recur;
   xdg.dataFile."task/dark-green-256.theme".source = ../taskwarrior/.local/share/task/dark-green-256.theme; # todo fetch from net
 
 }
