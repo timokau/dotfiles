b373415875f684c541f7846e3dfb987b7df60db6
Timo Kaufmann <timokau@zoho.com>
2020-11-30 10:02:29 +0100

    Revert "nixpkgs: update"
    This reverts commit 728352186b4bf42f337723c6b03bba14e75b8444.
    
    The update broke my desktop's xserver after a reboot (segfault). Haven't
    had time to investigate much yet. I'm just glad I could do a rollback
    and get back to a workings state for now.

--- nixpkgs.nix
+++ nixpkgs.nix
@@ -1,6 +1,6 @@
 let
   # `git ls-remote https://github.com/nixos/nixpkgs-channels nixos-unstable`
-  nixpkgs-rev = "6f0c00907bbd81b47052eef59f7b284926a77289";
+  nixpkgs-rev = "2247d824fe07f16325596acc7faa286502faffd1";
 in builtins.fetchTarball {
   name = "nixpkgs-${nixpkgs-rev}";
   url = "https://github.com/nixos/nixpkgs/archive/${nixpkgs-rev}.tar.gz";
