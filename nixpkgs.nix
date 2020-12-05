6cc0ea2ff7a32a96a9d22ade2c3cb8bb839cfd7b
Timo Kaufmann <timokau@zoho.com>
2020-12-05 15:55:57 +0100

    nixpkgs: update
    The xorg crash was fixed by the xorgserver 1.20.10 update.

--- nixpkgs.nix
+++ nixpkgs.nix
@@ -1,6 +1,6 @@
 let
   # `git ls-remote https://github.com/nixos/nixpkgs-channels nixos-unstable`
-  nixpkgs-rev = "2247d824fe07f16325596acc7faa286502faffd1";
+  nixpkgs-rev = "7ba8820008a9fc767377bbc5f67bf85aee514bee";
 in builtins.fetchTarball {
   name = "nixpkgs-${nixpkgs-rev}";
   url = "https://github.com/nixos/nixpkgs/archive/${nixpkgs-rev}.tar.gz";
