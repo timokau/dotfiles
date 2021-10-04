3ee3397e3b30fdac8c17f240746e60c8778fd1b9
Timo Kaufmann <timokau@zoho.com>
2021-10-04 22:53:07 +0200

    Move taskwarrior config to config home
    Taskwarrior now supports configuration files in XDG_CONFIG_HOME.

--- home/taskwarrior.nix
+++ home/taskwarrior.nix
@@ -11,7 +11,7 @@
     recursive = true;
     source = ../taskwarrior/.config/task;
   };
-  home.file.".taskrc".source = ../taskwarrior/taskrc; # points to the ~/.config folder
+  home.file.".config/task/taskrc".source = ../taskwarrior/taskrc;
   home.file.".local/share/task/hooks/on-modify.relative-recur".source = ../taskwarrior/hooks/on-modify.relative-recur;
   xdg.dataFile."task/dark-green-256.theme".source = ../taskwarrior/.local/share/task/dark-green-256.theme; # todo fetch from net
 
