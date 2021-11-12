bf74592f2bc4ca2c528c27bdcaea0c998ab20053
Timo Kaufmann <timokau@zoho.com>
2021-11-12 19:27:17 +0100

    nixpkgs: update
    This includes the nix 2.4 update, which requires some changes to the
    rebuild script.

--- nixpkgs.nix
+++ nixpkgs.nix
@@ -1,6 +1,6 @@
 let
   # `git ls-remote https://github.com/nixos/nixpkgs-channels nixos-unstable`
-  nixpkgs-rev = "9e70e9f732fbb15872cb4ea11bd43c14328a0b69";
+  nixpkgs-rev = "c5ed8beb478a8ca035f033f659b60c89500a3034";
 in builtins.fetchTarball {
   name = "nixpkgs-${nixpkgs-rev}";
   url = "https://github.com/nixos/nixpkgs/archive/${nixpkgs-rev}.tar.gz";
--- rebuild.sh
+++ rebuild.sh
@@ -9,18 +9,27 @@ read
 # you remove a file from this list, you should delete it manually.
 sudo install --owner=root --group=root --mode=644 nixos/*.nix nixos/server_address nixpkgs.nix /etc/nixos
 
+nix_cmd() {
+	nix --extra-experimental-features nix-command "$@"
+}
+nixpkgs_hm() {
+	# Run home-manager from the nixpkgs in NIX_PATH.
+	nix_cmd run -f '<nixpkgs>' home-manager "$@"
+}
+
 echo "Rebuilding"
 
-NIXPKGS="$(nix eval --raw '(import ./nixpkgs.nix)')"
+NIXPKGS="$(nix_cmd eval --raw --file ./nixpkgs.nix)"
 # set nixpkgs in NIX_PATH explicitly once, then it gets encoded as the default in the system configuration
 export NIX_PATH="nixpkgs=$NIXPKGS:$NIX_PATH"
 
 # First build both systems using nix 2
 echo "Instantiating"
-home_manager_system="$( nix run nixpkgs.home-manager -c home-manager --show-trace instantiate )"
+home_manager_system="$( nixpkgs_hm --show-trace instantiate )"
+nixos_system="$( nix_cmd eval --impure --raw --expr '((import <nixpkgs/nixos>) {}).system' )"
 [[ $? -ne 0 ]] && exit $?
 echo "Rebuilding"
-nix build --no-link --show-trace '(with import <nixpkgs/nixos> { }; system)' $home_manager_system || exit $?
+nix_cmd build --no-link --show-trace $nixos_system $home_manager_system || exit $?
 
 echo "Waiting for attention: Permission to switch"
 while ! sudo echo "Sudo password cached"; do :; done
@@ -34,4 +43,4 @@ sudo nixos-rebuild -I nixpkgs="$NIXPKGS" switch
 systemctl --user restart keyboardconfig
 
 echo "Switching home"
-nix run nixpkgs.home-manager -c home-manager switch
+nixpkgs_hm switch
