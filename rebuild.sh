f3b5771c2d657b678d458e2a5b5643e4f3457a29
Timo Kaufmann <timokau@zoho.com>
2021-11-20 16:57:08 +0100

    Use the old nix CLI in the rebuild script
    Using the new CLI makes it a pain to switch between old and new
    configurations since nix is currently taken from the environment. An old
    configuration would have an older version of nix and couldn't run the
    rebuild script.
    
    Using the old CLI avoid this for now since newer nix versions still
    support it.
    
    It would be nicer to take the nix version from the pinned nixpkgs, but
    that poses bootstrapping problems.

--- rebuild.sh
+++ rebuild.sh
@@ -9,27 +9,19 @@ read
 # you remove a file from this list, you should delete it manually.
 sudo install --owner=root --group=root --mode=644 nixos/*.nix nixos/server_address nixpkgs.nix /etc/nixos
 
-nix_cmd() {
-	nix --extra-experimental-features nix-command "$@"
-}
-nixpkgs_hm() {
-	# Run home-manager from the nixpkgs in NIX_PATH.
-	nix_cmd run -f '<nixpkgs>' home-manager "$@"
-}
-
 echo "Rebuilding"
 
-NIXPKGS="$(nix_cmd eval --raw --file ./nixpkgs.nix)"
+NIXPKGS="$(nix-instantiate --eval ./nixpkgs.nix | tr --delete '"' )"
 # set nixpkgs in NIX_PATH explicitly once, then it gets encoded as the default in the system configuration
 export NIX_PATH="nixpkgs=$NIXPKGS:$NIX_PATH"
 
-# First build both systems using nix 2
+# First build both systems together for increased parallelization
 echo "Instantiating"
-home_manager_system="$( nixpkgs_hm --show-trace instantiate )"
-nixos_system="$( nix-instantiate -E '(with import <nixpkgs/nixos> {}; system)' )"
+home_manager_system="$( nix-shell --packages home-manager --run 'home-manager --show-trace instantiate' )"
+nixos_system="$( nix-instantiate -E '((import <nixpkgs/nixos>) {}).system' )"
 [[ $? -ne 0 ]] && exit $?
 echo "Rebuilding"
-nix_cmd build --no-link --show-trace $nixos_system $home_manager_system || exit $?
+nix-build --no-link --show-trace $nixos_system $home_manager_system || exit $?
 
 echo "Waiting for attention: Permission to switch"
 while ! sudo echo "Sudo password cached"; do :; done
@@ -43,4 +35,4 @@ sudo nixos-rebuild -I nixpkgs="$NIXPKGS" switch
 systemctl --user restart keyboardconfig
 
 echo "Switching home"
-nixpkgs_hm switch
+nix-shell --packages home-manager --run 'home-manager --show-trace switch'
