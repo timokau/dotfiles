e42de6bdb3d031c761d3c6fdfdaaeefd1cd5bd45
Timo Kaufmann <timokau@zoho.com>
2022-01-19 15:12:09 +0100

    Enable TLP on the laptop

--- nixos/hosts/pad.nix
+++ nixos/hosts/pad.nix
@@ -10,6 +10,23 @@
   # mount /tmp in RAM. Don't do this on pad, as the machine tends to run out of ram.
   boot.tmpOnTmpfs = true;
 
+  services.tlp = {
+    enable = true;
+    settings = {
+      # Stop charging at 80%, start again at 75%.
+      START_CHARGE_THRESH_BAT0=75;
+      STOP_CHARGE_THRESH_BAT0=80;
+      START_CHARGE_THRESH_BAT1=75;
+      STOP_CHARGE_THRESH_BAT1=80;
+      # Thresholds may be changed temporarily (fullcharge). Restore them when unplugging.
+      RESTORE_THRESHOLDS_ON_BAT=1;
+    };
+  };
+  # Required by TLP for ThinkPad battery calibration
+  boot = {
+    kernelModules = ["acpi_call"];
+    extraModulePackages = with config.boot.kernelPackages; [ acpi_call ];
+  };
 
   services.xserver.dpi = 120;
 
