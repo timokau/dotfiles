9c8cde190a9c2529774ee26b439fb61ab9a9908a
Timo Kaufmann <timokau@zoho.com>
2022-01-21 14:47:25 +0100

    Reduce resource usage of syncthing

--- home/configuration.nix
+++ home/configuration.nix
@@ -346,6 +346,13 @@ with pkgs.lib; {
       # TODO add config
       enable = true;
     };
+    # Wrap syncthing with various commands to keep its resource usage low.
+    systemd.user.services.syncthing.Service.ExecStart = pkgs.lib.mkForce (concatStringsSep " " [
+      "${pkgs.coreutils}/bin/env GOMAXPROCS=1" # only one core (https://docs.syncthing.net/users/faq.html#why-does-it-use-so-much-cpu)
+      "${pkgs.coreutils}/bin/nice --adjustment=19" # minimum scheduling priority
+      "${pkgs.util-linux}/bin/ionice --class 2 --classdata 7" # "best effort" io scheduling with lowest priority
+      "${pkgs.syncthing}/bin/syncthing -no-browser -no-restart -logflags=0"
+    ]);
 
     xdg.configFile."tridactyl/tridactylrc".source = ../tridactyl/.config/tridactyl/tridactylrc;
 
