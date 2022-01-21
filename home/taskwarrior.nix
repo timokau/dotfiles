e1cc0a54b5181a5d48d9153a90177a328f6dafa9
Timo Kaufmann <timokau@zoho.com>
2022-01-21 14:56:18 +0100

    Directly move taskrc to config folder

--- home/taskwarrior.nix
+++ home/taskwarrior.nix
@@ -11,7 +11,6 @@
     recursive = true;
     source = ../taskwarrior/.config/task;
   };
-  home.file.".config/task/taskrc".source = ../taskwarrior/taskrc;
   home.file.".local/share/task/hooks/on-modify.relative-recur".source = ../taskwarrior/hooks/on-modify.relative-recur;
   xdg.dataFile."task/dark-green-256.theme".source = ../taskwarrior/.local/share/task/dark-green-256.theme; # todo fetch from net
 
