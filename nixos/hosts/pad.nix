e190b79725e06b9efbf8344a0c3bd01c80cbb766
Timo Kaufmann <timokau@zoho.com>
2022-01-21 14:11:03 +0100

    Automatically switch profile when mic is requested

--- nixos/hosts/pad.nix
+++ nixos/hosts/pad.nix
@@ -38,6 +38,12 @@
   services.blueman.enable = true;
   programs.dconf.enable = true; # used by blueman
   hardware.pulseaudio = {
+    configFile = pkgs.runCommand "default.pa" {} ''
+      # Automatically switch audio profile when microphone access is requested
+      # We need to modify the default configuration, since it already loads
+      # module-bluetooth-policy
+      sed 's/load-module module-bluetooth-policy$/load-module module-bluetooth-policy auto_switch=2/' ${pkgs.pulseaudio}/etc/pulse/default.pa > "$out"
+    '';
     extraConfig = ''
       # Automatically switch the default to a new sink or source. Useful for
       # bluetooth headsets.
