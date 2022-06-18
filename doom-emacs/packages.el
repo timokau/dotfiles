89ffb3f76f81f927d963d844999830ae88ec7c28
Timo Kaufmann <timokau@zoho.com>
2022-06-18 08:59:06 +0200

    Update org-appear

--- doom-emacs/packages.el
+++ doom-emacs/packages.el
@@ -42,7 +42,7 @@
 ; Mix fixed and variable pitch as appropriate
 (package! mixed-pitch :pin "519e05f74825abf04b7d2e0e38ec040d013a125a")
 ; Make it easy to reveal hidden content such as link locations
-(package! org-appear :pin "8dd1e564153d8007ebc4bb4e14250bde84e26a34")
+(package! org-appear :pin "60ba267c5da336e75e603f8c7ab3f44e6f4e4dac")
 (package! org-modern :pin "b352680d9ee8c6e6966cc21d595513c3595e3bb7")
 
 
