4a024842f5846b91d05a6190094656c7d5d55307
Timo Kaufmann <timokau@zoho.com>
2022-08-14 12:10:38 +0200

    Initialize yoga host configuration

---
+++ nixos/hosts/yoga.nix
@@ -0,0 +1,21 @@
+{ config
+, pkgs
+, lib
+, ...
+}:
+
+{
+  imports = [
+    ./battery.nix
+    ./bluetooth.nix
+    ./intel-graphics.nix
+    ./touchpad.nix
+  ];
+  networking.hostName = "yoga";
+
+  boot.loader.efi.efiSysMountPoint = "/boot/efi";
+
+  # The NixOS release to be compatible with for stateful data such as databases.
+  system.stateVersion = "22.05";
+}
+
