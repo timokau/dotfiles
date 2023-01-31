bb5c925705d05ff28e85125bec78b0aeb3d7080a
Timo Kaufmann <timokau@zoho.com>
2023-01-31 17:32:51 +0100

    Migrate to new openssh settings format

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -184,8 +184,10 @@ in
 
   services.openssh = {
     enable = true;
-    passwordAuthentication = false;
-    permitRootLogin = "no";
+    settings = {
+      PasswordAuthentication = false;
+      PermitRootLogin = "no";
+    };
     ports = [ 2143 ];
   };
 
