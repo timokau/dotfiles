232ea08eb3ccae16fb89f970e149530b52befe18
Timo Kaufmann <timokau@zoho.com>
2023-02-02 17:05:40 +0100

    Add libreoffice to home pkgs

--- home/configuration.nix
+++ home/configuration.nix
@@ -67,6 +67,7 @@ with pkgs.lib; {
       };
     in with pkgs; [
       zotero # reference management
+      libreoffice # office suite
       easyeffects # audio effects, such as microphone noise reduction
       ripgrep-all # search non-text files such as pdfs
       pdfpc # latex beamer presentations
