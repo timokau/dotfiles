6442e2bd3630cca945239eaecd90ca9818e3d38a
Timo Kaufmann <timokau@zoho.com>
2023-07-31 17:14:04 +0200

    Revert "nixpkgs: update"
    This reverts commit 95abe48115160c99cc1c521e8000c55dd62f3234.

--- nixpkgs.nix
+++ nixpkgs.nix
@@ -1,6 +1,6 @@
 let
   # `git ls-remote https://github.com/nixos/nixpkgs-channels nixos-unstable`
-  nixpkgs-rev = "b12803b6d90e2e583429bb79b859ca53c348b39a";
+  nixpkgs-rev = "f2406198ea0e4e37d4380d0e20336c575b8f8ef9";
 in builtins.fetchTarball {
   name = "nixpkgs-${nixpkgs-rev}";
   url = "https://github.com/nixos/nixpkgs/archive/${nixpkgs-rev}.tar.gz";
