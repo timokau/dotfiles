de44933bee92bef55443fd4343b667545848cfec
Timo Kaufmann <timokau@zoho.com>
2023-09-04 16:33:21 +0200

    Backport herbstluftwm patch for recent xorg compat

--- home/herbstluftwm.nix
+++ home/herbstluftwm.nix
@@ -23,8 +23,20 @@ with lib;
     };
 
     xsession.enable = true;
-    xsession.windowManager.command = ''
-      PATH="${pkgs.herbstluftwm}/bin:$PATH" herbstluftwm
+    xsession.windowManager.command = 
+    let
+      # Fix tests with recent xorg versions.
+      # https://github.com/NixOS/nixpkgs/pull/253271
+      patched-hlwm = pkgs.herbstluftwm.overrideAttrs (old: {
+        patches = old.patches ++ [
+          (pkgs.fetchpatch {
+            url = "https://github.com/herbstluftwm/herbstluftwm/commit/1a6e8ee24eac671569f54bfec22ab47ff285a52c.patch";
+            hash = "sha256-srulWJQ9zTR4Kdxo40AdHND4nexDe2PDSR69yWsOpVA=";
+          })
+        ];
+      });
+    in ''
+      PATH="${patched-hlwm}/bin:$PATH" herbstluftwm
     '';
   };
 }
