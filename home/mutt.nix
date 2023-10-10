1115b6afab54a6561a1df393a60b2a086e641e6a
Timo Kaufmann <timokau@zoho.com>
2023-10-10 14:56:41 +0200

    Add mutt process binding

--- home/mutt.nix
+++ home/mutt.nix
@@ -15,5 +15,6 @@
   home.file.".mutt/gpg.rc".source = ../mutt/.mutt/gpg.rc;
   home.file.".mutt/mailcap".source = ../mutt/.mutt/mailcap;
   home.file.".mutt/muttrc".source = ../mutt/.mutt/muttrc;
+  home.file.".mutt/process".source = ../mutt/.mutt/process; # not in git repo
   home.file.".mutt/personal".source = ../mutt/.mutt/personal; # not in git repo
 }
