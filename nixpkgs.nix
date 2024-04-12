a6a5ec62076a20cfb55d4b22a5d13d80fa6096d0
Timo Kaufmann <timokau@zoho.com>
2024-04-12 16:45:46 +0200

    nixpkgs: update

--- nixpkgs.nix
+++ nixpkgs.nix
@@ -1,6 +1,6 @@
 let
   # `git ls-remote https://github.com/nixos/nixpkgs-channels nixos-unstable`
-  nixpkgs-rev = "4cba8b53da471aea2ab2b0c1f30a81e7c451f4b6";
+  nixpkgs-rev = "1042fd8b148a9105f3c0aca3a6177fd1d9360ba5";
 in builtins.fetchTarball {
   name = "nixpkgs-${nixpkgs-rev}";
   url = "https://github.com/nixos/nixpkgs/archive/${nixpkgs-rev}.tar.gz";
