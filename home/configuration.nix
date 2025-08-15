135cee2b486d60e910d97ef50042a3b84a8d5125
Timo Kaufmann <timokau@zoho.com>
2025-08-15 17:56:28 +0200

    Add readwise desktop apps

--- home/configuration.nix
+++ home/configuration.nix
@@ -417,6 +417,8 @@ with pkgs.lib; {
     xdg.dataFile."applications/ticktick.desktop".text = makeChromiumDesktopApp "TickTick" "https://ticktick.com/webapp/" "";
     xdg.dataFile."applications/openrouter_chat.desktop".text = makeChromiumDesktopApp "OpenRouter Chat" "https://www.openrouter.ai/chat" "";
     xdg.dataFile."applications/notion.desktop".text = makeChromiumDesktopApp "Notion" "https://www.notion.so/" "";
+    xdg.dataFile."applications/readwise_reader.desktop".text = makeChromiumDesktopApp "Readwise Reader" "https://read.readwise.io/home" "";
+    xdg.dataFile."applications/readwise_feed.desktop".text = makeChromiumDesktopApp "Readwise Feed" "https://read.readwise.io/feed" "";
     xdg.dataFile."applications/google-calendar.desktop".text = optionalString cfg.graphical ''
       [Desktop Entry]
       Encoding=UTF-8
