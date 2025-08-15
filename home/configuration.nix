aff01080a310962c2d83c081bd59cd478ab7ce51
Timo Kaufmann <timokau@zoho.com>
2025-08-15 17:54:41 +0200

    Add aistudio desktop app

--- home/configuration.nix
+++ home/configuration.nix
@@ -411,6 +411,7 @@ with pkgs.lib; {
     xdg.dataFile."applications/figma.desktop".text = makeChromiumDesktopApp "Figma" "https://www.figma.com/" "";
     xdg.dataFile."applications/brain.fm.desktop".text = makeChromiumDesktopApp "Brain.fm" "https://www.brain.fm/app" "";
     xdg.dataFile."applications/gemini.desktop".text = makeChromiumDesktopApp "Gemini" "https://gemini.google.com/app" "";
+    xdg.dataFile."applications/aistudio.desktop".text = makeChromiumDesktopApp "Google AI Studio" "https://aistudio.google.com/prompts/new_chat" "";
     xdg.dataFile."applications/google-calendar.desktop".text = optionalString cfg.graphical ''
       [Desktop Entry]
       Encoding=UTF-8
