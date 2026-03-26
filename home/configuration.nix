0e05eb39bcd46d630592860495f61830755c4c00
Timo Kaufmann <timokau@zoho.com>
2026-03-26 17:13:55 +0100

    Update pins

--- home/configuration.nix
+++ home/configuration.nix
@@ -86,19 +86,8 @@ with pkgs.lib; {
         src = sources.nix-ai-tools.outPath;
       }).defaultNix.outputs.packages.${pkgs.stdenv.hostPlatform.system};
 
-      # Update less to 692 to fix kitty terminal input regression in 686-691
-      # https://github.com/NixOS/nixpkgs/pull/482131#issuecomment-4020142255
-      less-692 = assert lib.versionOlder pkgs.less.version "692";
-        pkgs.less.overrideAttrs (old: rec {
-          version = "692";
-          src = pkgs.fetchurl {
-            url = "https://www.greenwoodsoftware.com/less/less-${version}.tar.gz";
-            hash = "sha256-YTAPYDeY7PHXeGVweJ8P8/WhrPB1pvufdWg30WbjfRQ=";
-          };
-        });
-
     in with pkgs; [
-      less-692
+      less
       nix-ai-tools-pkgs.claude-code
       ripgrep-all # search through various files (mainly pdfs)
       zip # creating archives
